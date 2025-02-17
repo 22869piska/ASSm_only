dbaee13587f4d9e9cab170d20b83708841874c5b
22869piska <164668282+22869piska@users.noreply.github.com>
2025-02-17 21:18:15 +0300

    Add files via upload

--- y_ASSM/main.asm
+++ y_ASSM/main.asm
@@ -1,25 +1,58 @@
 include main.inc
 
+
+
+;------***************************************** proto func 
+learn proto 
+prava_proc proto
+
 ;=======================
 .DATA                  
+       
+       msg_caption db "READ",0
+       msg_text db "Remove capslock and choose english layout",0
+
+
+       _negr SMALL_RECT<0,0,100,35>;<140,80>
+
 
 
 
 .CODE 
 start:
-
-CALL main                    ;; *тут происходит выход когда main end
+CALL main                   
 invoke ExitProcess,0
-
-;----------------------
+;---------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  main proc
+  CALL prava_proc
 
+  fn MessageBox,0, addr msg_text, addr msg_caption, MB_OK
+  ;--
   CALL learn
 
-
-
-inkey
-;----------------------
+  inkey
 ret
-main endp ;MAIN END
+main endp 
+;=====================================================================
+;_______________________________________________________________________________________________________
+learn proc uses EBX ESI EDI
+
+       lea esi,_negr
+      ;---
+      fn SetConsoleTitle,"VELIKIY ASSM"
+      fn windowsize_proc,80,80
+      fn hide_cursor
+      ;---
+   
+      CALL game
+
+
+;--
+ RET
+learn endp 
+;----------------------------------------------------------------------------------------------------------------------------
+
+
+
+;=====================================================================
 end start
